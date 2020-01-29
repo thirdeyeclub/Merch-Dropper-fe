bf14b6309961fe89a4828db656b474c4b452c0bc
Jeris Manning <the.goose.moose@gmail.com>
2020-01-28 17:54:25 -0700

    form linked with local state

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
 import NavBar from './components/NavBar';
+import Registration from './components/Registration';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <h1>Labs 20 Merch Dropper</h1>
       <NavBar />
+      <Registration />
     </div>
   );
 }
