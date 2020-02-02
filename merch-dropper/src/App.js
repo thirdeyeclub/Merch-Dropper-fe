255f4aaebce3556a35952dc9ce21058d87fa88b5
tboudreau614 <54781883+tboudreau614@users.noreply.github.com>
2020-02-01 18:51:20 -0700

    footer pass 1

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -3,12 +3,14 @@ import React from 'react';
 import NavBar from './components/NavBar';
 import './App.css';
 import Homepage from './components/Homepage';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Homepage />
+      <Footer />
     </div>
   );
 }
