fe74c20baca5f306c2654163127600e68083fe75
Marcos Lira <liram1995@gmail.com>
2020-02-05 16:17:20 -0600

    Added authentication w/ bugs...

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -1,10 +1,11 @@
 import React,{ useState } from 'react';
-
+import { Route } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import './App.css';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
 import data from './dummyData';
+import Callback from './components/Callback';
 
 function App() {
   const [products] = useState(data)
@@ -15,6 +16,7 @@ function App() {
       <NavBar />
       <ShoppingCart />
       <Products product={products}/>
+      <Route exact path="/callback" component={Callback} exact />
     </div>
   );
 }
--- merch-dropper/src/components/NavBar.js
+++ merch-dropper/src/components/NavBar.js
@@ -1,4 +1,5 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import auth0Client from "./Auth";
 import '../App.css';
 import {
   Collapse,
@@ -21,34 +22,101 @@ import {
 const NavBar = (props) => {
   const [isOpen, setIsOpen] = useState(false);
 
+// useEffect(() => {
+//   if (localStorage.getItem("Id_token")) {
+//     auth0Client.signIn();
+//   }
+// }, [])
+
   const toggle = () => setIsOpen(!isOpen);
 
-  return (
-    <div class="divNav">
-      <Navbar color="white" light expand="md" className="navStyle">
-        <img className="mr-5" src="https://uxmasters.org/images/merch_logo_50.svg" />
-        <NavbarBrand id="navTitle" href="/">Merch Dropper</NavbarBrand>
-        <NavbarToggler onClick={toggle} />
-        <Collapse isOpen={isOpen} navbar>
-          <Nav className="mr-auto" navbar>
-            <NavItem>
-            <FormGroup className="searchStyle pt-3">
-              <Input className="rounded-pill"
-                  type="search"
-                  name="search"
-                  id="exampleSearch"
-                  placeholder="Search... "
-              />
-            </FormGroup>
-            </NavItem>
 
-          </Nav>
-            <Button color="primary" href="/" className="designBtn">Design Merch</Button>{' '}
-            <Button className="ml-5" outline color="primary" href="/">Buy Merch</Button>{' '}
-        </Collapse>
-      </Navbar>
-    </div>
-  );
-}
+  const signOut = () => {
+    auth0Client.signOut();
+    this.props.history.replace("/");
+  };
 
-export default NavBar;
+  if (auth0Client.isAuthenticated()) {
+    return (
+      <div className="divNav">
+        <Navbar color="white" light expand="md" className="navStyle">
+          <img
+            className="mr-5"
+            src="https://uxmasters.org/images/merch_logo_50.svg"
+          />
+          <NavbarBrand id="navTitle" href="/">
+            Merch Dropper
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <FormGroup className="searchStyle pt-3">
+                  <Input
+                    className="rounded-pill"
+                    type="search"
+                    name="search"
+                    id="exampleSearch"
+                    placeholder="Search... "
+                  />
+                </FormGroup>
+              </NavItem>
+            </Nav>
+            <Button color="primary" href="/" className="designBtn">
+              Design Merch
+            </Button>{" "}
+            <Button className="ml-5" outline color="primary" href="/">
+              Buy Merch
+            </Button>{" "}
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+  else {
+    return (
+      <div class="divNav">
+        <h1>Nothing here...</h1>
+        <Navbar color="white" light expand="md" className="navStyle">
+          <img
+            className="mr-5"
+            src="https://uxmasters.org/images/merch_logo_50.svg"
+          />
+          <NavbarBrand id="navTitle" href="/">
+            Merch Dropper
+          </NavbarBrand>
+          <NavbarToggler onClick={toggle} />
+          <Collapse isOpen={isOpen} navbar>
+            <Nav className="mr-auto" navbar>
+              <NavItem>
+                <FormGroup className="searchStyle pt-3">
+                  <Input
+                    className="rounded-pill"
+                    type="search"
+                    name="search"
+                    id="exampleSearch"
+                    placeholder="Search... "
+                  />
+                </FormGroup>
+              </NavItem>
+            </Nav>
+            <Button color="primary" href="/" className="designBtn">
+              Design Merch
+            </Button>{" "}
+            <Button className="ml-5" outline color="primary" href="/">
+              Buy Merch
+            </Button>{" "}
+            <Button className="ml-5" onClick={auth0Client.signIn}>
+              Sign In
+            </Button>
+            <Button className="ml-5" onClick={auth0Client.signOut}>
+              Sign Out
+            </Button>
+          </Collapse>
+        </Navbar>
+      </div>
+    );
+  }
+    }
+    
+    export default NavBar;
