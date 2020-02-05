a6b0b58f991f50fea9d2106c24eeb188391b5dd0
Jeris Manning <the.goose.moose@gmail.com>
2020-02-05 14:32:07 -0700

    restructure and add merch card component

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -10,6 +10,7 @@ import NavBar from "./components/NavBar";
 import Registration from "./components/Registration";
 import Shirt from "./components/ShirtComponents/Shirt";
 import ShirtMaker from "./components/ShirtComponents/ShirtMaker";
+import ShirtsCard from "./components/Layout/ShirtCard/ShirtsCard";
 import "./App.css";
 >>>>>>> working on shirt building and display functionality
 
@@ -28,9 +29,9 @@ function App() {
     <div className="App">
       {/* <h1>Labs 20 Merch Dropper</h1> */}
       <NavBar />
-      <Registration />
+      {/* <Registration /> */}
       <Shirt />
-
+      <ShirtsCard />
 
       {/* <ShirtDisplay /> */}
     </div>
