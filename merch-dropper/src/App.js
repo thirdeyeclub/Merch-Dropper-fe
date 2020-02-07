7c3c2db47d4fe31675c3f377c7839b93c2018f8e
Oliver-Strange <preston.burton@gmail.com>
2020-02-07 14:28:27 -0800

    removes double declaration of shoppingcart in app.js

--- merch-dropper/src/App.js
+++ merch-dropper/src/App.js
@@ -1,17 +1,15 @@
-import React,{ useState } from 'react';
-import { Route } from 'react-router-dom';
-import NavBar from './components/NavBar';
-import './App.css';
-import Products from './components/Products';
-import ShoppingCart from './components/ShoppingCart';
-import data from './dummyData';
-import Callback from './components/Callback';
-import Home from './components/Home';
+import React, { useState } from "react";
+import { Route } from "react-router-dom";
+import NavBar from "./components/NavBar";
+import "./App.css";
+import Products from "./components/Products";
+import ShoppingCart from "./components/ShoppingCart";
+import data from "./dummyData";
+import Callback from "./components/Callback";
+import Home from "./components/Home";
 import Homepage from "./components/Homepage";
 import Footer from "./components/Footer";
 
-
-import ShoppingCart from "./components/ShoppingCart";
 import initialState from "./reducers/initialState";
 
 import ProductDisplay from "./components/ProductDisplay";
@@ -23,8 +21,6 @@ function App() {
     <div className="App">
       <Route exact path="/" component={Home} exact />
       <Route exact path="/callback" component={Callback} exact />
-      {/* <NavBar /> */}
-      {/* <Homepage /> */}
       <ShoppingCart />
 
       <ProductDisplay products={products} />
